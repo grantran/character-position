d9529bd36ff0ef1787c19c178aaf562cf91168b6
Grant Tran <grant.tran@gmail.com>
2017-05-05 19:18:19 +0000

    changed capitalization and removed spaces

--- character-position.js
+++ character-position.js
@@ -1,51 +1,19 @@
 function LettersPosition(str) {
   var output = {};
-  var strArr = str.split('')
-  //var char = noSpace.split('');
-  //console.log(strArr);
-  //console.log(char.length);
+  var strArr = str.toLowerCase().split('')
 
-///////////////////////////////
-
-
-  // for (i = 0; i < strArr.length; i++)
-  //   output[strArr[i]] = [];
-
-  // console.log(output);
-
-  // for (i = 0; i < strArr.length; i++) {
-  //   output[strArr[i]].push(i);
-
-  // console.log(output);
   for (i = 0; i < strArr.length; i++) {
-    if (strArr[i] in output) {
-      output[strArr[i]].push(i);
+    if (strArr[i] != " ") {
+      if (strArr[i] in output) {
+        output[strArr[i]].push(i);
 
-    }
-    else {
-      output[strArr[i]] = [i];
-      //console.log(output[char[i]]);
+      }
+      else {
+        output[strArr[i]] = [i];
+      }
     }
   }
   return output;
   }
 
-
-
-
 console.log(LettersPosition('Lighthouse in the house'));
-
-
-  // for (i = 0; i < strArr.length; i++) {
-  //   if (strArr[i] in output) {
-  //     output.strArr[i] = push(strArr.indexOf(strArr[i]));
-  //     //console.log(output[char[i]]);
-  //   }
-  //   else {
-  //     output[strArr[i]] = [];
-  //     output[strArr[i]].push(strArr.indexOf(strArr[i]));
-  //     //console.log(output[char[i]]);
-  //   }
-  // }
-
-  // return output;
